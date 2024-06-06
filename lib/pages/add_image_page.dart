ee85e65d914483c49defa9043b3fafdb118ea3d5
Hasegawa-Akito <h.akito1999@gmail.com>
2024-06-06 22:35:35 +0900

    futureの再生成防止

--- lib/pages/add_image_page.dart
+++ lib/pages/add_image_page.dart
@@ -20,6 +20,9 @@ class AddImagePage extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final localPhotos = useState<List<AssetEntity>>([]);
+    final thumbnailFutures =
+        useState<Map<int, Future<Uint8List?>>>(<int, Future<Uint8List?>>{});
+    final selectedPhoto = useState<AssetEntity?>(null);
 
     void setLocalPhotos() async {
       try {
@@ -31,8 +34,20 @@ class AddImagePage extends HookWidget {
 
         final List<AssetEntity> images = await fetchLocalPhotos();
         localPhotos.value = images;
+
+        // futureオブジェクトはFutureBuilderの外で生成する
+        // これはuseEffectで初回しか呼ばれないため、ここでfutureオブジェクトを生成しておくことで
+        // それ以外での不要な生成を防ぐ
+        // FutureBuilderの中で生成すると、不要なタイミングでfutureオブジェクトが再生成される。
+        final Map<int, Future<Uint8List?>> futures = {};
+        for (int i = 0; i < images.length; i++) {
+          futures[i] =
+              images[i].thumbnailDataWithSize(const ThumbnailSize(200, 200));
+        }
+        thumbnailFutures.value = futures;
+
+        selectedPhoto.value = images[0];
       } catch (e) {
-        print('Error: $e');
         return;
       }
     }
@@ -52,29 +67,66 @@ class AddImagePage extends HookWidget {
       return null;
     }, []);
 
+    // futureオブジェクトはFutureBuilderの外で生成する。
+    // useMemoizedは適切なタイミングでしか、実行されないため、不要なfutureオブジェクトの再生成を行わない
+    // FutureBuilderの中で生成すると不要な再生成がされる
+    final selectedPhotoFuture = useMemoized(() {
+      return selectedPhoto.value
+          ?.thumbnailDataWithSize(const ThumbnailSize(600, 600));
+    }, [selectedPhoto.value]);
+
     return Scaffold(
-      appBar: AppBar(title: Text('Gallery')),
-      body: GridView.builder(
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 3,
-          mainAxisSpacing: 5,
-          crossAxisSpacing: 5,
-        ),
-        itemCount: localPhotos.value.length,
-        itemBuilder: (context, index) {
-          return FutureBuilder<Uint8List?>(
-            future: localPhotos.value[index].thumbnailDataWithSize(
-              const ThumbnailSize(200, 200),
+      appBar: AppBar(title: Text('画像選択')),
+      body: Column(
+        children: [
+          if (selectedPhoto.value != null)
+            FutureBuilder<Uint8List?>(
+              future: selectedPhotoFuture,
+              builder: (context, snapshot) {
+                final bytes = snapshot.data;
+                if (bytes == null) {
+                  return Center(child: CircularProgressIndicator());
+                }
+                return Container(
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.height * 4 / 7,
+                  child: Image.memory(bytes, fit: BoxFit.cover),
+                );
+              },
             ),
-            builder: (context, snapshot) {
-              final bytes = snapshot.data;
-              if (bytes == null) {
-                return Center(child: CircularProgressIndicator());
-              }
-              return Image.memory(bytes, fit: BoxFit.cover);
-            },
-          );
-        },
+          Expanded(
+            child: Padding(
+              padding:
+                  const EdgeInsets.only(top: 10.0), // Add padding to the top
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  mainAxisSpacing: 5,
+                  crossAxisSpacing: 5,
+                ),
+                itemCount: localPhotos.value.length,
+                itemBuilder: (context, index) {
+                  final future = thumbnailFutures.value[index];
+                  return FutureBuilder<Uint8List?>(
+                    future: future,
+                    builder: (context, snapshot) {
+                      final bytes = snapshot.data;
+                      if (bytes == null) {
+                        return Center(child: CircularProgressIndicator());
+                      }
+                      return GestureDetector(
+                        onTap: () {
+                          selectedPhoto.value = localPhotos.value[index];
+                        },
+                        child: Image.memory(bytes, fit: BoxFit.cover),
+                      );
+                    },
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
