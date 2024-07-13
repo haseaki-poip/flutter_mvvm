aa08391c7969c8a49712b5305b83e016142c1e4d
Hasegawa-Akito <h.akito1999@gmail.com>
2024-07-13 20:53:35 +0900

    Fix typo

--- lib/repository/memory_repository.dart
+++ lib/repository/memory_repository.dart
@@ -2,5 +2,5 @@ import 'package:mvvm_training/model/memory_model.dart';
 
 abstract class MemoryRepository {
   Future<List<MemoryModel>> find();
-  Future<void> save(List<MemoryModel> mmemoryList);
+  Future<void> save(List<MemoryModel> memoryList);
 }
